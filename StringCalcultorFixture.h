94d95d92036cc46678cf75ce93435754eddabf92
kerenhappuch441 <91006012+kerenhappuch441@users.noreply.github.com>
2024-10-28 17:00:14 +0530

    Update StringCalcultorFixture.h

--- StringCalcultorFixture.h
+++ StringCalcultorFixture.h
@@ -1,26 +1,29 @@
 
-#include "StringCalculator.cpp"
+#include "StringCalculator.h"
 #include <gtest/gtest.h>
-
-
-class stringCalculatorAddfixture:public testing::Test
-{
-StringCalculator objectundertest;
-void setup() overidden
-{
-  objectundertest = new StringCalculator();
-}
-void teardown()
-{
-  delete objectundertest;
-}
+ 
+class StringCalculatorAddFixture: public testing::Test{
+  protected: 
+    StringCalculator *objUnderTest;
+  void SetUp() override {
+  objUnderTest = new StringCalculator();
+  }
+  void TearDown(){
+    delete objUnderTest;
+  }
 };
-
-Test_F(stringCalculatorAddfixture,Returns_0_For_Empty)
-{
-  string input="";
-  int expectedvalue=0;
-  int actualvalue=objectundertest->Add(input);
-  AssertEQ(actualvalue,expectedvalue);
+ 
+TEST_F(StringCalculatorAddFixture, Returns_0_For_Empty){
+  string input = "";
+  int expectedValue = 0;
+  int actualValue = objUnderTest->Add(input);
+  ASSERT_EQ(actualValue, expectedValue);
+}
+ 
+TEST_F(StringCalculatorAddFixture, Returns_0_For_0){
+  string input = "0";
+  int expectedValue = 0;
+  int actualValue = objUnderTest->Add(input);
+  ASSERT_EQ(actualValue, expectedValue);
 }
 
