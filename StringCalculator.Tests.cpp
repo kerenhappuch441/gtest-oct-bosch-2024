4a9caa8a08ac5f1722f6a638b516cf3b76a3703c
kerenhappuch441 <91006012+kerenhappuch441@users.noreply.github.com>
2024-10-28 14:45:00 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,1 +1,16 @@
-
+#include "StringCalculator.h"
+#include <gtest/gtest.h>
+ 
+TEST(string_calculator_ass,when_passed_a_single_number_returns_0_for_Empty){
+//Arrange
+  StringCalculator  objUnderTests;
+  string input = "";
+  int expectedValue = 0;
+ 
+ 
+  //ACT
+  int actualValue = objUnderTest.Add(input);
+ 
+  //ASSERT
+  ASSERT_EQ(actualValue,0);
+}
