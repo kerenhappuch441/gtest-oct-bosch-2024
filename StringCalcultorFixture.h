cecb71a6f333341c6dadada70257a0eddc4dd5e9
kerenhappuch441 <91006012+kerenhappuch441@users.noreply.github.com>
2024-10-28 16:47:24 +0530

    Update StringCalcultorFixture.h

--- StringCalcultorFixture.h
+++ StringCalcultorFixture.h
@@ -1,1 +1,22 @@
 
+#include "StringCalculator.h"
+#include <gtest/gtest.h>
+
+
+class stringCalculatorAddfixture:public testing::Test
+{
+Stringcalculator objectundertest;
+
+void setup() overidden
+{
+  objectundertest = new Stringcalculator();
+
+}
+void teardown()
+{
+  delete objectundertest;
+}
+
+
+
+}
