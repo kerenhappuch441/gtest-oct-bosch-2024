4685f05a6bb95150dc429938aaca342f0f2f54c7
kerenhappuch441 <91006012+kerenhappuch441@users.noreply.github.com>
2024-10-29 10:55:45 +0530

    Update StringCalculatorValueParameterTest.cpp

--- StringCalculatorValueParameterTest.cpp
+++ StringCalculatorValueParameterTest.cpp
@@ -19,7 +19,7 @@ class StringCalculatorAddParameterizedFixture:
                                     public testing::WithParamInterface<tuple<string,int>>{ 
 };
 INSTANTIATE_TEST_SUITE_P(ValidValuesDataSet,StringCalculatorAddParameterizedFixture,testing::Values(
-  make_tuple("",0),make_tuple("0",0),make_tuple("1",1),make_tuple("1,2",3),make_tuple("//[**][%^]\n4**1%^9", 14),make_tuple("//;\n1;2"), 3),make_tuple("42,1001,3",45),make_tuple("//[***]\n8***2***3", 13),make_tupled("//[*][%]\n4*2%3", 9) 
+  make_tuple("",0),make_tuple("0",0),make_tuple("1",1),make_tuple("1,2",3),make_tuple("//[**][%^]\n4**1%^9", 14),make_tuple("//;\n1;2", 3),make_tuple("42,1001,3",45),make_tuple("//[***]\n8***2***3", 13),make_tupled("//[*][%]\n4*2%3", 9) 
 ));
 INSTANTIATE_TEST_SUITE_P(NegetiveValuesDataSet,StringCalculatorAddParameterizedFixture,testing::Values(
   make_tuple("1,-2,-4,5", std::invalid_argument)
@@ -31,7 +31,10 @@ TEST_P(StringCalculatorAddParameterizedFixture, DataDrivenTestCase){
     int actualValue=objUnderTest->Add(input);
     ASSERT_EQ(actualValue,expectedValue);
 }
-TEST_F(StringCalculatorAddFixture, Throw_Invalid_Argumnet_Exceptio_For_Negetive_Numbers){
-    string input="1,-2";
-    EXPECT_THROW(objUnderTest->Add(input),invalid_argument);
-  }
+ 
+TEST_P(StringCalculatorAddParameterizedFixture, DataDrivenNegativeTestCase){
+    string input=std::get<0>(GetParam());
+    int expectedValue=std::get<1>(GetParam());
+    int actualValue=objUnderTest->Add(input);
+    ASSERT_EQ(actualValue,expectedValue);
+}
